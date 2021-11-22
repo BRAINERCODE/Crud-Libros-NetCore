b7f9bc8507eda4e4a78598ed6cdad58eb1d33272
Brainer Esteban Betancur Gonzalez <brainer.betancur@masglobalconsulting.com>
2021-11-22 13:00:58 -0500

    initial commit

---
+++ MicroserviceUser/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MicroserviceUser.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MicroserviceUser/Models/Libro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroserviceUser.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+        //le muestra al usuario que es obligatorio
+        [Required(ErrorMessage = "el titulo es obligatorio")]
+        //Obliga al usuario a tener entre 3 y 50 caracteres y si no cumple nos arroja un mensaje
+        [StringLength(50, ErrorMessage = "EL {0} debe ser al menos {2} y maximo {1} caracteres", MinimumLength = 3)]
+        //Esto es por si necesitamos que sea con tilde pero en DB no tiene tilde
+        [Display(Name = "Tìtulo")]
+
+        public string titulo { get; set; }
+
+        [Required(ErrorMessage = "la descripcion es obligatorio")]
+        [StringLength(50, ErrorMessage = "EL {0} debe ser al menos {2} y maximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "Descripciòn")]
+
+        public string descripcion { get; set; }
+
+        [Required(ErrorMessage = "la fecha es obligatorio")]
+        //Aca le damos formato a la hora, solo queremos la fecha
+        [DataType(DataType.Date)]
+
+        public DateTime fechaDeLanzamiento { get; set; }
+
+        [Required(ErrorMessage = "el autor es obligatorio")]
+        public string Autor { get; set; }
+        [Required(ErrorMessage = "el precio es obligatorio")]
+
+        public int precio { get; set; }
+    }
+}
